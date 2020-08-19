3ebe5aabf50c9287732113f50616326d4a862b75
sowish23 <soso2266@naver.com>
2020-08-20 00:27:23 +0900

    edit form datepicker 추가

--- myCareerApp/forms.py
+++ myCareerApp/forms.py
@@ -1,13 +1,18 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Profile, ProjectBoard
 from .widgets import DatePickerWidget
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
 
 class ProfileForm(ModelForm):
     class Meta:
         model = Profile
         fields = ['image', 'name', 'intro', 'tel', 'email', 'birth', 'tech', 'career', 'kakaoId', 'github_url']
         widgets = {
-            'birth' : DatePickerWidget
+            'birth' : DateInput()
         }
 
 class ProjectForm(ModelForm):
