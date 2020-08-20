bab3f469cc7570e043ccbb55ece63f5fd47c782b
sowish23 <soso2266@naver.com>
2020-08-21 04:15:28 +0900

    로그인 및 회원 가입 구현

--- myCareerApp/admin.py
+++ myCareerApp/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import ProjectBoard, Profile, User
+from .models import ProjectBoard, Profile
 
 admin.site.register(ProjectBoard)
 admin.site.register(Profile)
-admin.site.register(User)
+# admin.site.register(User)
 
 class UserAdmin(admin.ModelAdmin):
     
