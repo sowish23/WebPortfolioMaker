ff13de704424bf1c574ce6612ecf9b6ea0dc1c37
sowish23 <soso2266@naver.com>
2020-09-01 00:40:11 +0900

    user 생성 시 profile 자동 생성

--- myCareerApp/models.py
+++ myCareerApp/models.py
@@ -65,7 +65,7 @@ class Profile(models.Model):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        Profile.objects.create(user_id=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
