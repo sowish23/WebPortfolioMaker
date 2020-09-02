90c42039548e3f0acd524dbca4dc7198369d34bc
sowish23 <soso2266@naver.com>
2020-09-03 01:34:53 +0900

    project user_id 부여

--- myCareerApp/models.py
+++ myCareerApp/models.py
@@ -4,50 +4,6 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-# class UserManager(BaseUserManager):    
-#     use_in_migrations = True
-#     def create_user(self, email, nickname, password=None):
-#         if not email :            
-#             raise ValueError('must have user email')        
-#         user = self.model(            
-#             email = self.normalize_email(email),            
-#             nickname = nickname        
-#         )
-#         user.set_password(password)        
-#         user.save(using=self._db)        
-#         return user     
-#     def create_superuser(self, email, nickname, password ):
-#         user = self.create_user(
-#             email = self.normalize_email(email),            
-#             nickname = nickname,            
-#             password=password        
-#         )        
-#         user.is_admin = True        
-#         user.is_superuser = True        
-#         user.is_staff = True        
-#         user.save(using=self._db)        
-#         return user 
-
-# class User(AbstractBaseUser,PermissionsMixin): 
-#     objects = UserManager()
-    
-#     email = models.EmailField(        
-#         max_length=255,        
-#         unique=True,    
-#     )    
-#     nickname = models.CharField(
-#         max_length=20,
-#         null=False,
-#         unique=True
-#     )     
-#     is_active = models.BooleanField(default=True)    
-#     is_admin = models.BooleanField(default=False)    
-#     is_superuser = models.BooleanField(default=False)    
-#     is_staff = models.BooleanField(default=False)     
-#     date_joined = models.DateTimeField(auto_now_add=True)     
-#     USERNAME_FIELD = 'nickname'    
-#     REQUIRED_FIELDS = ['email']
-
 class Profile(models.Model):
     objects = models.Manager()
     user_id = models.OneToOneField(User, null=True, on_delete=models.CASCADE, verbose_name="USERID")
@@ -73,8 +29,9 @@ def save_user_profile(sender, instance, **kwargs):
 
 class ProjectBoard(models.Model):
     objects = models.Manager()
-    project_title = models.CharField(null=False, default = '', max_length=200, verbose_name="PROJECT TITLE")
-    project_des = models.TextField(null=False, default = '', verbose_name="PROJECT DESCRIPTION")
-    project_url = models.TextField(null=False, default = '', verbose_name="PROJECT URL")
-    project_img = models.ImageField(upload_to = "static/img/project", null=False, default = '', verbose_name="PROJECT IMAGE")
-    project_id = models.AutoField(db_column='ProjectId', primary_key=True)
+    user_id = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    project_title = models.CharField(null=True, blank=True, default = '', max_length=200, verbose_name="PROJECT TITLE")
+    project_des = models.TextField(null=True, blank=True, default = '', verbose_name="PROJECT DESCRIPTION")
+    project_url = models.TextField(null=True, blank=True, default = '', verbose_name="PROJECT URL")
+    project_img = models.ImageField(upload_to = "static/img/project", null=True, blank=True, default = '', verbose_name="PROJECT IMAGE")
+    project_num = models.AutoField(db_column='ProjectId', primary_key=True)
